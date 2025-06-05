5de9352af339e8847aba0a5988fbdb0a3158e19c
danielleit241 <danielleee241@gmail.com>
2025-06-05 23:28:14 +0700

    Add project files.

---
+++ SchoolMedicalWpf.App/LoginWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace SchoolMedicalWpf.App
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SchoolMedicalWpf.UnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SchoolMedicalWpf.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
