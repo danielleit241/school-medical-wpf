de3dfb4a6354b34b614279e16c48ce5159d1cfb1
Phuong Hoa <danielleee241@gmail.com>
2025-06-14 02:06:50 +0700

    Add files via upload

---
+++ SchoolMedicalWpf.Dal/Repositories/RoleRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolMedicalWpf.Dal.Entities;
+using SchoolMedicalWpf.Dal;
+
+public class RoleRepository
+{
+    private readonly IDbContextFactory<SchoolmedicalWpfContext> _contextFactory;
+
+    public RoleRepository(IDbContextFactory<SchoolmedicalWpfContext> contextFactory)
+    {
+        _contextFactory = contextFactory;
+    }
+
+    public async Task<List<Role>> GetAllRoles()
+    {
+        using var context = _contextFactory.CreateDbContext();
+        return await context.Roles.ToListAsync();
+    }
+
+    public async Task<Role?> GetRoleById(int roleId)
+    {
+        using var context = _contextFactory.CreateDbContext();
+        return await context.Roles
+            .Include(r => r.Users)
+            .FirstOrDefaultAsync(r => r.RoleId == roleId);
+    }
+}
